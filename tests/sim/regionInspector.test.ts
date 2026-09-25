0c4363f690035d0b801ffd15fc1081ed55a79880
zeusmonsterx <kaustuvmx25@gmail.com>
2026-09-25 13:08:39 +0530

    test #809 runtime lineage identity: tests/sim/regionInspector.test.ts

--- tests/sim/regionInspector.test.ts
+++ tests/sim/regionInspector.test.ts
@@ -108,13 +108,13 @@ describe('authoritative region inspector', () => {
     expect(inspection.totalBiomass).toBe(21)
     expect(inspection.lineageBiomass).toEqual([
       {
-        lineageId: 'ancestor',
+        lineageId: 'L1',
         genotypeId: 'WT',
         biomass: 6,
         fractionOfRegionBiomass: 6 / 21,
       },
       {
-        lineageId: 'variant',
+        lineageId: 'L2',
         genotypeId: 'VAR',
         biomass: 15,
         fractionOfRegionBiomass: 15 / 21,
@@ -199,13 +199,13 @@ describe('authoritative region inspector', () => {
     expect(inspection.totalBiomass).toBe(0)
     expect(inspection.lineageBiomass).toEqual([
       {
-        lineageId: 'ancestor',
+        lineageId: 'L1',
         genotypeId: 'WT',
         biomass: 0,
         fractionOfRegionBiomass: 0,
       },
       {
-        lineageId: 'variant',
+        lineageId: 'L2',
         genotypeId: 'VAR',
         biomass: 0,
         fractionOfRegionBiomass: 0,
